5f35e7bce2eee08218f4cd67f73a0a37fff7ebca
huscse <husnainkhaliq82@gmail.com>
2025-08-13 21:34:39 -0400

    changed prompt

--- src/db/index.js
+++ src/db/index.js
@@ -7,11 +7,10 @@ const connectDB = async () => {
             `${process.env.MONGO_URI}/${DB_NAME}`
         );
         console.log(
-            `\n MongoDB connected!! DB Host: ${connectionInstance.connection.host}`
+            `\n MongoDB connection successful! DB Host: ${connectionInstance.connection.host}`
         );
     } catch (error) {
         console.log("MONGODB connection FAILED: ", error);
-
         process.exit(1);
     }
 };
