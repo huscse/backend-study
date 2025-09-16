b17d742eee46f385fdc93446ba668cdd6234a747
huscse <husnainkhaliq82@gmail.com>
2025-09-16 12:35:48 -0400

    removed temp edits

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ const app = express()(async () => {
         app.listen(process.env.PORT, () => {
             console.log(`App is listening on port: , ${process.env.PORT}`);
             console.log(`App working!!)
-            temp edit
+            
         }); 
 
     } catch (error) {
